223a8c94da30cf28aaa85261251adbd589189b14
Alina-Grin <ao@radiogps.ru>
2020-11-25 16:09:40 +0300

    removing

--- errors.c
+++
@@ -1,9 +0,0 @@
-#include "ft_select.h"
-
-void	handle_errors(char *description)
-{
-	ft_putstr_fd("ft_select: ", STDERR_FILENO);
-	ft_putstr_fd(description, STDERR_FILENO);
-	leave_tty();
-	exit(EXIT_FAILURE);
-}
